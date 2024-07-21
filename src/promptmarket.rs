aa43d35f56761335d239f4f49f45a9f3b7e338f1
F80 <43803343+f80dev@users.noreply.github.com>
2024-07-21 23:52:19 +0200

    ajout de cancel_server et evaluations

--- src/promptmarket.rs
+++ src/promptmarket.rs
@@ -13,25 +13,25 @@ use multiversx_sc::imports::*;
 pub struct Prompt<M: ManagedTypeApi> {
     pub text:ManagedBuffer<M>,          //contenu du prompt ou address pointant sur un prompt
     pub owner:usize,                    //Propriétaire
-    pub start:u32,                       //Date de lancement
+    //pub start:u32,                       //Date de lancement
     pub server:usize                    //Server demandé
 }
 
 #[derive(TypeAbi, TopEncode, TopDecode,NestedEncode,NestedDecode)]
 pub struct Server<M: ManagedTypeApi> {
+    pub id:usize,                    //
     pub title:ManagedBuffer<M>,             //Promotion du moteur
+    pub domain:ManagedBuffer<M>,             //Domain du serveur
     pub price:BigUint<M>,               //Montant proposé
-    pub token:EgldOrEsdtTokenIdentifier<M>,
-    pub model:u16,                       //Modele demandé
-    pub inference:u16,                  //Inférence
-    pub scale:u16,                      //résolution
+    pub token:TokenIdentifier<M>,
+    pub parameters:ManagedBuffer<M>,             //Parametrage du serveur
     pub owner:usize,                    //Propriétaire
 }
 
 #[derive(TypeAbi,TopEncode, TopDecode,NestedEncode,NestedDecode)]
 pub struct Eval {
-    pub server:usize,
-    pub user:usize,
+    pub render:usize,           //Rendu recevant la note
+    pub user:usize,             //Auteur de la note
     pub note:u8
 }
 
@@ -42,6 +42,8 @@ pub struct Render<M: ManagedTypeApi> {
     pub prompt_id:usize,
     pub url:ManagedBuffer<M>,
     pub creator:usize,
+    pub start_delay:u8,                 //Delai en minutes pour démarrer le rendu
+    pub render_duration:u8              //Durée en minutes pour le rendu
 }
 
 
@@ -133,22 +135,22 @@ pub trait PromptMarket {
         //voir https://github.com/multiversx/mx-nft-collection-minter-sc/blob/c198141d2436f41b5b3afcea24c3ebbb23d3f13b/nft-minter/src/brand_creation.rs#L148
 
         let server=self.servers().get_by_index(server_id);
-
-        let token_payment: EgldOrEsdtTokenPayment = self.call_value().egld_or_single_esdt();
-        require!(token_payment.token_identifier==server.token,"Ne correspond pas à la monnaie du server");
-        require!(token_payment.amount==server.price,"Ne correspond pas au prix du server");
-
+        if server.price>0 {
+            let token_payment: EgldOrEsdtTokenPayment = self.call_value().egld_or_single_esdt();
+            require!(token_payment.token_identifier==server.token,"Ne correspond pas à la monnaie du server");
+            require!(token_payment.amount==server.price,"Ne correspond pas au prix du server");
+        }
 
         //TODO Ajouter le controle du nombre max de prompt par wallet
 
-        require!(text.len() > 4,"La prompts doit faire au moins 5 caractères");
+        require!(text.len() > 10,"La prompts doit faire au moins 5 caractères");
 
-        let delay=self.blockchain().get_block_timestamp()-self.dt_start_market().get();
+        //let delay=self.blockchain().get_block_timestamp()-self.dt_start_market().get();
 
         let prompt = Prompt {
             text: text,
             owner:self.add_address(&self.blockchain().get_caller()),
-            start:u32::try_from(delay).unwrap(),
+            //start:u32::try_from(delay).unwrap(),
             server: server_id,
         };
         self.prompts().insert(prompt);
@@ -156,40 +158,50 @@ pub trait PromptMarket {
     }
 
 
-    #[endpoint]
-    fn cancel_prompt(&self,prompt_id:usize) -> bool {
-        let caller=self.blockchain().get_caller();
+    fn close_prompt(&self,prompt_id:usize,owner:usize) -> bool {
+        require!(self.closed_prompt().get_index(&prompt_id)==0,"Ce prompt est déjà clôt");
+
         let prompt=self.prompts().get_by_index(prompt_id);
-        require!(prompt.owner==self.add_address(&caller),"Vous n'êtes pas propriétaire de ce prompt");
+        require!(prompt.owner==owner,"Vous n'êtes pas propriétaire de ce prompt");
 
         //Fermeture du prompt
         self.closed_prompt().insert(prompt_id);
 
         //Remboursement
         let server=self.servers().get_by_index(prompt.server);
-        self.send().direct(
-            &caller,
-            &server.token, 0,
-            &server.price
-        );
-
+        if server.price>0 {
+            self.send().direct_esdt(
+                &self.users().get_by_index(owner),
+                &server.token, 0,
+                &server.price
+            );
+        }
         return true;
     }
 
-        #[endpoint]
-    fn add_server(&self,title:ManagedBuffer,model:u16,inference:u16,scale:u16,price:BigUint,token: EgldOrEsdtTokenIdentifier) -> usize {
-        require!(model > 0u16,"Le modele n'est pas correct");
-        require!(inference > 10,"Un minimum de 10 inférences est requis");
-        require!(scale > 64,"la taille minimum est de 64 pixels");
+
+    #[endpoint]
+    fn cancel_prompt(&self,prompt_id:usize) -> bool {
+        let caller=self.add_address(&self.blockchain().get_caller());
+        return self.close_prompt(prompt_id,caller);
+    }
+
+
+    #[endpoint]
+    fn add_server(&self,title:ManagedBuffer,domain:ManagedBuffer,params:ManagedBuffer,price:BigUint,token: TokenIdentifier) -> usize {
+        // require!(model > 0u16,"Le modele n'est pas correct");
+        // require!(inference > 10,"Un minimum de 10 inférences est requis");
+        // require!(scale > 64,"la taille minimum est de 64 pixels");
+        require!(domain.len() > 8,"Invalid domain address");
 
         let server = Server {
+            id:self.servers().len()+1,
             title:title,
+            domain:domain,
             price: price,
             token: token,
-            model: model,
-            inference:inference,
             owner:self.add_address(&self.blockchain().get_caller()),
-            scale:scale
+            parameters: params
         };
         self.servers().insert(server);
         return self.servers().len()
@@ -240,16 +252,33 @@ pub trait PromptMarket {
     fn cancel_server(&self, server_id:usize) -> bool {
         require!(server_id<=self.servers().len(),"Server inexistant");
         require!(self.closed_servers().get_index(&server_id)==0,"Server déjà clos");
+        let owner=self.add_address(&self.blockchain().get_caller());
+        let server=self.servers().get_by_index(server_id);
+        require!(server.owner==owner,"Vous n'êtes pas propriétaire de ce server de rendu");
+
+        for (i,p) in self.prompts().iter().enumerate() {
+            if p.server==server_id {
+                if !self.closed_prompt().contains(&i) {
+                    self.close_prompt(i,p.owner);
+                }
+
+            }
+        }
         return self.closed_servers().insert(server_id)
     }
 
+
     #[endpoint]
-    fn eval_server(&self, server_id:usize,note:u8)  -> bool {
-        require!(server_id<=self.servers().len(),"Server inexistant");
-        require!(note>0 && note<=20,"Server déjà clos");
+    fn eval_render(&self, render_id:usize,note:u8)  -> bool {
+        require!(render_id<=self.renders().len(),"Render unknown");
+        require!(note>0 && note<=20,"note between 1 and 20");
+        let user=self.add_address(&self.blockchain().get_caller());
+        let prompt=self.prompts().get_by_index(self.renders().get_by_index(render_id).prompt_id);
+        require!(prompt.owner==user,"Vous ne pouvez pas évaluer ce rendu");
+
         let eval=Eval {
-            server: server_id,
-            user: self.add_address(&self.blockchain().get_caller()),
+            render: render_id,
+            user: user,
             note: note
         };
         return self.evals().insert(eval)
@@ -259,7 +288,7 @@ pub trait PromptMarket {
 
 
     #[endpoint]
-    fn add_render(&self, prompt_id:usize,url:ManagedBuffer) -> usize {
+    fn add_render(&self, prompt_id:usize,url:ManagedBuffer,render_duration:u8,start_dealy:u8) -> usize {
         /*
         Ajout d'un rendu avec son prix
         */
@@ -278,16 +307,21 @@ pub trait PromptMarket {
             url:url,
             prompt_id:prompt_id,
             creator:creator,
+            render_duration:render_duration,
+            start_delay:start_dealy
         };
         self.renders().insert(render);
 
         //Paiement https://docs.multiversx.com/developers/developer-reference/sc-api-functions/#direct_esdt
         //Paiement du créateur
-        self.send().direct(
-            &caller,
-            &server.token, 0,
-            &server.price
-        );
+        if server.price>0 {
+
+            self.send().direct_esdt(
+                &caller,
+                &server.token, 0,
+                &server.price
+            );
+        }
 
         //Fermeture du prompt
         self.closed_prompt().insert(prompt_id);
